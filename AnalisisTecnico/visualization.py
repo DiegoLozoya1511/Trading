273ad7a7564f3b3c9a4d1cd540a93393237f89d4
Diego Lozoya Morales <diego.lozoya@gmail.com>
2025-10-23 19:41:20 -0600

    add visualization module and .gitignore; initialize DeepLearningIntro notebook

---
+++ AnalisisTecnico/visualization.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.facecolor'] = 'lightgrey'
+plt.rcParams['figure.figsize'] = (12, 6)
+plt.rcParams['axes.grid'] = True
+plt.rcParams['grid.alpha'] = 0.5
+plt.rcParams['grid.linestyle'] = '--'
+plt.rcParams['axes.titlesize'] = 16
+plt.rcParams['axes.labelsize'] = 12
+plt.rcParams['legend.frameon'] = True
+plt.rcParams['legend.facecolor'] = 'white'
+plt.rcParams['legend.edgecolor'] = 'black'
+
+
+colors = ["cornflowerblue", "indianred", "darkseagreen", "plum", "dimgray"]
